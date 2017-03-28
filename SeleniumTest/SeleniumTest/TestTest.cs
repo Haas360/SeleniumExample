38ac224962c5c81c59bdc920e3919347c27fe7b2
Kamil Hadas <hadas.kamil@gmail.com>
2017-03-28 18:14:19 +0200

    starting project

---
+++ SeleniumTest/SeleniumTest/TestTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumTest
+{
+    [TestFixture]
+    public class TestTest
+    {
+        [Test]
+        public void Test()
+        {
+            //            System.Environment.SetEnvironmentVariable("webdriver.gecko.driver", "C:\\webdriver\\geckodriver.exe");
+
+            using (IWebDriver driver = new FirefoxDriver())
+            {
+                driver.Navigate().GoToUrl(new Uri("http://www.mitfristed.dk"));
+            }
+            
+            
+
+        }
+    }
+}
